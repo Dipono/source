ff2f885bd92a615df67b7e109ada42c6ee400619
unknown <joel.buti97@gmail.com>
2024-02-05 19:56:34 +0200

    fetch api added

--- map/public/main.js
+++ map/public/main.js
@@ -21,8 +21,8 @@ async function initializeMap() {
 async function main() {
     // Create an authentication helper instance using an API key
     const authHelper = await amazonLocationAuthHelper.withAPIKey(apiKey);
-   
-    
+
+
     // Initialize map and Amazon Location SDK client:
     const map = await initializeMap();
 
@@ -36,56 +36,44 @@ async function main() {
 
     // On mouse click, display marker and get results:
     // map.on("click", async function (e) {
-    // Remove any existing marker
-    if (marker) {
-        marker.remove();
-    }
-    //var coordinate = [{ long: 25.08504994, lat: -28.997182288 }, { long: 24.08504994, lat: -27.997182288 }, { long: 28.2292712, lat: -25.7478676 }]
-    // Render a marker on clicked point
-    fetch('http://localhost:3001/api/coordinates').then(results =>
-    results.json()).then(respond => {
-        console.log(respond)
-        if (respond.success) {
-            console.log(respond.results)
-            for (var k = 0; k < respond.results.length; k++) {
-                marker = new maplibregl.Marker().setLngLat([respond.results[k].long, respond.results[k].lat]).addTo(map);
-            }
+        // Remove any existing marker
+        if (marker) {
+            marker.remove();
         }
-        else {
-
+        var coordinate = [{long:25.08504994, lat:-28.997182288}, {long:24.08504994, lat:-27.997182288}, {long:28.2292712, lat:-25.7478676}]
+        // Render a marker on clicked point
+        for(var k = 0; k < coordinate.length; k++){
+            marker = new maplibregl.Marker().setLngLat([coordinate[k].long, coordinate[k].lat]).addTo(map);
         }
+        
+/*
+        // Set up parameters for search call
+        let params = {
+            IndexName: placesName,
+            Position: [25.08504994, -28.997182288],
+            Language: "en",
+            MaxResults: "5",
+        };
 
-    })
-    
+        // Set up command to search for results around clicked point
+        const searchCommand = new amazonLocationClient.SearchPlaceIndexForPositionCommand(params);
 
-    /*
-            // Set up parameters for search call
-            let params = {
-                IndexName: placesName,
-                Position: [25.08504994, -28.997182288],
-                Language: "en",
-                MaxResults: "5",
-            };
-    
-            // Set up command to search for results around clicked point
-            const searchCommand = new amazonLocationClient.SearchPlaceIndexForPositionCommand(params);
-    
-            try {
-                // Make request to search for results around clicked point
-                const data = await client.send(searchCommand);
-    
-                // Write JSON response data to HTML
-                document.querySelector("#response").textContent = JSON.stringify(data, undefined, 2);
-    
-                // Display place label in an alert box
-                alert(data.Results[0].Place.Label);
-            } catch (error) {
-                // Write JSON response error to HTML
-                document.querySelector("#response").textContent = JSON.stringify(error, undefined, 2);
-    
-                // Display error in an alert box
-                alert("There was an error searching.");
-            }*/
+        try {
+            // Make request to search for results around clicked point
+            const data = await client.send(searchCommand);
+
+            // Write JSON response data to HTML
+            document.querySelector("#response").textContent = JSON.stringify(data, undefined, 2);
+
+            // Display place label in an alert box
+            alert(data.Results[0].Place.Label);
+        } catch (error) {
+            // Write JSON response error to HTML
+            document.querySelector("#response").textContent = JSON.stringify(error, undefined, 2);
+
+            // Display error in an alert box
+            alert("There was an error searching.");
+        }*/
     // });
 }
 
